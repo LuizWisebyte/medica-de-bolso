7dfb4664b5ef62d03a2778ac8c12dab2230b78a0
Your Name <you@example.com>
2025-04-13 03:16:20 -0300

    Criação da classe MedicaDeBolsoApplication com configuração inicial do Spring Boot, incluindo habilitação de execução assíncrona e exclusão de filtros para pacotes de controladores específicos.

--- src/test/java/br/com/medicadebolso/MedicaDeBolsoApplication.java
+++ src/test/java/br/com/medicadebolso/MedicaDeBolsoApplication.java
@@ -2,12 +2,19 @@ package br.com.medicadebolso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@ComponentScan(excludeFilters = {
+    @ComponentScan.Filter(type = FilterType.REGEX, pattern = "br\\.com\\.medicadebolso\\.controller\\..*"),
+    @ComponentScan.Filter(type = FilterType.REGEX, pattern = "br\\.com\\.medicadebolso\\.api\\.controller\\..*")
+})
 public class MedicaDeBolsoApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(MedicaDeBolsoApplication.class, args);
     }
 } 
